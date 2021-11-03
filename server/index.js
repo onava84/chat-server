69e9655cf8ccecf0b2a21e1b82635dded0f36aa3
onava84 <oscar_nf@hotmail.com>
2021-11-03 10:07:09 -0500

    done

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
-const express = require('express')
-const app = express()
-const mc = require('./controllers/messages_controller')
-const PORT = 3001
-app.use(express.json())
+const express = require("express");
+const mc = require("./controllers/messages_controller");
+
+const app = express();
+
+app.use(express.json());
 app.use(express.static(__dirname + '/../public/build'));
 
-app.post('/api/messages', mc.create)
-app.get('/api/messages', mc.read)
-app.put('/api/messages/:id', mc.update)
-app.delete('/api/messages/:id', mc.delete)
+const messagesBaseUrl = "/api/messages";
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
 
-app.listen(PORT, () => {
-   console.log(`The server is running on port ${PORT}`)
-})
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
